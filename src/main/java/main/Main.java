4b7fdc8b1a1802c4cf96d88eb0b50b098c1e2315
Efren leonarso sosa <efrenl.sosab@gmail.com>
2021-06-17 23:38:39 -0500

    se agrega funcionalidad de conexion a base de datos, clases DTO y la clase consulta

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,47 @@
 package main;
 
+import connection.ConnectionDb;
+import consulta.Consulta;
+import dto.ActorDto;
+import dto.AddressDto;
+import dto.Category;
+
+import java.sql.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+         ConnectionDb conexion = new ConnectionDb();
+        Consulta consulta = new Consulta();
+         ActorDto actorDto = new ActorDto();
+        AddressDto addressDto = new AddressDto();
+        Category category = new Category();
+
+
+        conexion.conectarBD();
+        // consulta tabla actor y pasar a DTO correspondiente
+        actorDto.setActor_idDto(consulta.consultaActor("actor_id"));
+        actorDto.setFirst_nameDto(consulta.consultaActor("first_name"));
+        actorDto.setLast_nameDto(consulta.consultaActor("last_name"));
+        // consulta tabla address y pasar a DTO correspondiente
+        addressDto.setAddressDto(consulta.consultaAddress("address"));
+        addressDto.setAddressDto(consulta.consultaAddress("district"));
+        addressDto.setAddressDto(consulta.consultaAddress("city_id"));
+        addressDto.setAddressDto(consulta.consultaAddress("postal_code"));
+        addressDto.setAddressDto(consulta.consultaAddress("phone"));
+        addressDto.setAddressDto(consulta.consultaAddress("location"));
+        // consulta tabla category y pasar a DTO correspondiente
+        category.setCategory_idDto(consulta.consultarCategory("category_id"));
+        category.setNameDto(consulta.consultarCategory("name"));
+        conexion.cerrarBD();
+
+
+
+
+
+
+
+
+
     }
 }
